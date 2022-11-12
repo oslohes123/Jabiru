f01dd4fbf8532f810446e17462fc16a51b03570c
Abbas Bin Vakas <abbasbinvakas@yahoo.com>
2022-11-12 22:51:16 +0000

    Create users model and manager

--- lessons/models.py
+++ lessons/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from .managers import CustomUserManager
 
 # Create your models here.
+
+class User(AbstractBaseUser, PermissionsMixin):
+    first_name = models.CharField(max_length=10)
+    last_name = models.CharField(max_length=10)
+    email = models.EmailField(unique=True)
+    role = models.CharField(default='Student', max_length=10)
+
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'email'
+    EMAIL_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name', 'role']
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
+
+    @property
+    def is_staff(self):
+        return self.is_admin
