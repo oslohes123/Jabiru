76a95afb8d00cc9a1bb4452d6e298d529fa3cdcd
oslohes123 <ashleytyagi@hotmail.com>
2022-11-16 18:51:25 +0000

    Test user login user

--- msms/urls.py
+++ msms/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from lessons import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('login/',views.login,name = "login")
+
 ]
