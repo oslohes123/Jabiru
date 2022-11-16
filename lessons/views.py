d8265b5b3984a4015369fc681a06416cc2a4c4d2
Ashley Tyagi <ashleytyagi@hotamil.com>
2022-11-16 18:48:25 +0000

    Add login page view to views file

--- lessons/views.py
+++ lessons/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from lessons.forms import LogInForm
+
+
 
 # Create your views here.
+
+def login(request):
+    form = LogInForm()
+    return render(request, 'login.html',{'LogInForm':form})
+
