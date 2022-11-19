fec49bc89010104f1c9d26ed4f07fa32d08a8f03
Ashley Tyagi <ashleytyagi@outlook.com>
2022-11-19 20:47:00 +0000

    Update login_user
    Edited the login_user view to correctly authenticate them into the login 
    Landing still needs to be addressed in another issue

--- lessons/views.py
+++ lessons/views.py
@@ -8,26 +8,31 @@ from lessons.forms import LogInForm
 
 # Create your views here.
 
-
+# TODO: For landing page put the name of the view(from urls.py) as the redirect_url
 def login_user(request):
     if request.method == 'POST':
         form = LogInForm(request.POST)
-        nextItem = request.POST.get('nextItem') or ''
         if form.is_valid():
             email = form.cleaned_data.get('email')
             password = form.cleaned_data.get('password')
             user = authenticate(email=email, password=password)
             if user is not None:
                 login(request, user)
-                redirect_url = nextItem or 'feed'
+                redirect_url = "" # Upcoming landingPage name needs to be insertedHere
+                print(redirect_url)
                 return redirect(redirect_url)
-        messages.add_message(request, messages.ERROR, "Invalid credentials try again")
+            else:
+                print("Wrong login ")
+                messages.add_message(request, messages.ERROR, "Invalid credentials try again")
+                return render(request, 'log_in.html', {'form': form, 'next': "landingPage"})
     else:
-        nextItem = request.GET.get('nextItem') or ''
-    form = LogInForm()
-    return render(request, 'log_in.html', {'form': form, 'nextItem': nextItem})
+        print("GET REQUEST here")
+        form = LogInForm()
+        return render(request, 'log_in.html', {'form': form})
 
 
 def home(request):
     return render(request, 'home.html')
 
+def tempLandingPage(request):
+    return render(request,"tempLandingPage.html")
