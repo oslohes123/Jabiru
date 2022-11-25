a63ea960ba2dc56879ba0f65b41c77a365541fcf
Abbas Bin Vakas <abbasbinvakas@yahoo.com>
2022-11-25 18:32:54 +0000

    Add file test_log_in_view and file test_log_in_form to test the log in

--- lessons/tests/forms/test_log_in_form.py
+++ lessons/tests/forms/test_log_in_form.py
@@ -1,21 +1,41 @@
+"""Unit tests of the log in form"""
 from django import forms
 from django.test import TestCase
 from lessons.forms import LogInForm
 
 class LogInUserTestForm(TestCase):
+    """Unit tests of the log in form."""
     def setUp(self):
-        self.formInput = {"email":"testing@email.com","password":"Testing123%"}
+        self.form_input = {"email": "testing@email.com","password": "Testing123%"}
 
     """Unit tests for logging in a user"""
     def test_form_contains_required_fields(self):
         form = LogInForm()
-        self.assertIn('email',form.fields)
-        self.assertIn('password',form.fields)
+        self.assertIn('email', form.fields)
+        self.assertIn('password', form.fields)
         password_field = form.fields["password"]
         self.assertTrue(isinstance(password_field.widget,forms.PasswordInput))
+    
     def test_form_accepts_valid_input(self):
-        form = LogInForm(data=self.formInput)
+        form = LogInForm(data=self.form_input)
         self.assertTrue(form.is_valid())
 
+    def test_form_rejects_blank_email(self):
+        self.form_input['email'] = ''
+        form = LogInForm(data=self.form_input)
+        self.assertFalse(form.is_valid())
 
-        
+    def test_form_rejects_blank_password(self):
+        self.form_input['password'] = ''
+        form = LogInForm(data=self.form_input)
+        self.assertFalse(form.is_valid())
+
+    def test_form_accepts_incorrect_email(self):
+        self.form_input['username'] = 'incorrect@email.com'
+        form = LogInForm(data=self.form_input)
+        self.assertTrue(form.is_valid())
+
+    def test_form_accepts_incorrect_password(self):
+        self.form_input['password'] = 'WrongPassword123%'
+        form = LogInForm(data=self.form_input)
+        self.assertTrue(form.is_valid())
