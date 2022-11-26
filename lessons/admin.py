e3332247a3f33135392ba5f538a623a50f8a09e9
arraf2211 <arrafrahman22@gmail.com>
2022-11-26 14:05:34 +0000

    added in lessons to the administation view

--- lessons/admin.py
+++ lessons/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import User
+from .models import User,Lesson
 
 # Register your models here.
 
@@ -11,3 +11,9 @@ class UserAdmin(admin.ModelAdmin):
         'email', 'first_name', 'last_name', 'role', 'is_active'
     ]
 
+@admin.register(Lesson)
+class LessonAdmin(admin.ModelAdmin):
+    list_display =[
+        'further_info','student','availability','lesson_numbers','duration',
+        'interval','approve_status'
+    ]
