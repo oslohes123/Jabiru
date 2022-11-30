170a7acda9bb3af4ddb7776130710c6aa5199ac1
oslohes123 <ashleytyagi@hotmail.com>
2022-11-30 20:04:19 +0000

    Add test for values for inputted Checking that the values that were inputted into the database were what we could retrieve

--- lessons/tests/views/test_make_request_view.py
+++ lessons/tests/views/test_make_request_view.py
@@ -1,8 +1,9 @@
 from django.contrib.auth import login
 from django.test import TestCase, Client
 from django.urls import reverse
-
+from lessons.constants import *
 from lessons.models import User,Lesson
+from django.forms.models import model_to_dict
 
 class MakeRequestTest(TestCase):
     def setUp(self):
@@ -12,8 +13,9 @@ class MakeRequestTest(TestCase):
             first_name='IamaStudent',
             last_name='Doe',
             password="",
-            role='Student'
+            role = student
         )
+        # set_password ensures a hash of the password is stored and not the plain-text password
         self.studentUser.set_password("Password123")
         self.studentUser.save()
         self.studentLogin = {
@@ -21,20 +23,27 @@ class MakeRequestTest(TestCase):
             "password":"Password123"
         }
         self.studentLesson = {
+            "id":1,
             "student":self.studentUser,
-            "availability":"availability",
+            "availability":"I am available on Tuesdays",
             "lesson_numbers":2,
             "duration":2,
             "interval":3,
-            "further_info":"FurtherInfoBit",
+            "further_info":"I will need extra help!",
             "approve_status":False
         }
+        self.c = Client()
+        self.c.post(reverse("login_user"), self.studentLogin, follow=True)
 
     def test_make_request_input(self):
-        c = Client()
-        c.post(reverse("login_user"),self.studentLogin,follow=True)
-        originalAmount = Lesson.objects.count()
-        c.post(self.url,self.studentLesson,follow=True)
-        newAmount = Lesson.objects.count()
-        self.assertEqual(originalAmount + 1, newAmount)
+        original_amount = Lesson.objects.count()
+        self.c.post(self.url,self.studentLesson,follow=True)
+        new_amount = Lesson.objects.count()
+        self.assertEqual(original_amount + 1, new_amount)
 
+    def test_values_for_test_are_correct(self):
+        self.c.post(self.url,self.studentLesson,follow=True)
+        inserted_lesson = model_to_dict(Lesson.objects.get(student = self.studentUser))
+        # Lessons.objects.get() returns the pk for the student.
+        self.studentLesson['student'] = self.studentLesson['student'].pk
+        self.assertDictEqual(inserted_lesson,self.studentLesson)
