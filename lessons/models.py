b5e2e709b38805659744a97f33056c917ffa8237
Jingyi Wang <3072019561@qq.com>
2022-12-04 22:45:28 +0000

    corrected an error in model

--- lessons/models.py
+++ lessons/models.py
@@ -56,13 +56,12 @@ class Lesson(models.Model):
     approve_status = models.BooleanField(default=False)
     objects = CustomLessonManager()
 
-
 class ApprovedBooking(models.Model):
     student = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     start_date = models.DateField(blank=False)
     day_of_the_week = models.CharField(blank=False,choices=day_of_the_week_choices,max_length=20)
     time_of_the_week = models.TimeField(blank=False)
-    total_lessons_count = models.PositiveIntegerField(blank=False, label="")
+    total_lessons_count = models.PositiveIntegerField(blank=False)
     duration = models.PositiveIntegerField(blank=False, choices=duration_choices)
     interval = models.PositiveIntegerField(blank=False, choices=interval_choices)
     assigned_teacher = models.CharField(max_length=50, blank=False)
@@ -72,7 +71,6 @@ class ApprovedBooking(models.Model):
 
     def total_price(self):
         return self.total_lessons_count * self.hourly_rate * self.duration/60
-
 
 class Invoice(models.Model):
     lesson_in_invoice = models.OneToOneField(ApprovedBooking, on_delete=models.CASCADE,blank=False)
